c6bbed00fb73b670f9ad9b8bfa9343ce81829e1e
SGY <guanyushen668@foxmail.com>
2026-06-24 17:54:59 +0800

    配置CORS白名单

--- backend/src/main/java/com/gowhere/config/WebConfig.java
+++ backend/src/main/java/com/gowhere/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/api/**")
-        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+        .allowedOrigins(
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://www.gowhere.bond",
+            "https://gowhere.bond",
+            "https://go-where-orcin.vercel.app")
         .allowedMethods("GET", "POST", "OPTIONS")
         .allowedHeaders("*");
   }
